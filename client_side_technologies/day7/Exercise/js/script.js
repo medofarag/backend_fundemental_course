32b93d02eb7873736d8af975459a4825bc4fe0e0
Mahmoud Farag <medofarag@proton.me>
2026-07-20 22:28:49 +0300

    add day7

---
+++ client_side_technologies/day7/Exercise/js/script.js
@@ -0,0 +1,51 @@
+// Exercise 1
+console.log("Welcome To JavaScript");
+console.log("My Name is Ahmed");
+console.log("I'm 22 years old");
+console.log("I Love Programming");
+console.log("-----------------------------------");
+
+// Exercise 2
+let name = "mahmoud";
+let age = 22;
+let city = "cairo";
+let university = "Al_Azhar";
+let isStudent = true;
+
+console.log(name);
+console.log(age);
+console.log(city);
+console.log(university);
+console.log(isStudent);
+console.log("-----------------------------------");
+
+// Exercise 3
+age = 25;
+console.log(age);
+console.log("-----------------------------------");
+
+// Exercise 4
+const country = "Egypt";
+console.log(country);
+console.log("-----------------------------------");
+
+// Exercise 5
+console.log(typeof 20);
+console.log(typeof "20");
+console.log(typeof true);
+console.log(typeof false);
+console.log(typeof null);
+console.log(typeof undefined);
+console.log("-----------------------------------");
+
+// Exercise 6
+let Student = {
+  Name: "Mahmoud",
+  Age: 20,
+  Faculty: "Systems and Computers Engineering",
+  GPA: 3.8,
+  isGraduated: false,
+};
+
+console.log(Student);
+console.log("-----------------------------------");
